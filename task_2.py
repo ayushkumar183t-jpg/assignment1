a648e33156df92e9246c2d6fbaa58a775ecccf8c
ayushkumar183t-jpg <ayushkumar183t@gmail.com>
2026-02-19 10:16:16 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,5 @@
+first_name=input("enter your first name:")
+last_name=input("enter your last name:")
+print("Hello,",first_name ,last_name,"! Welcome to Python")
+print("thank you for using my program")
+input("press any key to exit ")
